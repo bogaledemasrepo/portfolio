40ece5ef04d69417de37d3fd673be09371d3aeb1
Admin <kali@kali>
2025-01-21 10:19:36 +0300

    project link fixed

--- constants/projectsList.tsx
+++ constants/projectsList.tsx
@@ -1,12 +1,11 @@
-
 export const projects = [
-    {
-      title: "Niky Landing page",
-      discription: `Creative web and mobile developer with a strong background in
+  {
+    title: "Niky Landing page",
+    discription: `Creative web and mobile developer with a strong background in
                   graphic design and latest javascript framework reactjs, react
                   native ,and nextjs.`,
-      tubnail: "url('/big-shoe1.png')",
-      sourceCode: "#",
-      previewLink: "#",
-    },
-  ];
+    tubnail: "url('/big-shoe1.png')",
+    sourceCode: "https://github.com/Boagale/nikeylandingpage",
+    previewLink: "https://nikylandingpage.vercel.app/",
+  },
+];
