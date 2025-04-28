b52d83859665dc6f676cadd21a0a0e78f199b05e
Bogale Demas <bogidemas@gmail.com>
2025-04-28 07:02:17 +0300

    Dark mode fix

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -21,7 +21,7 @@ const Projects = () => {
     previewLink?: string;
   };
 
-  const [projects, setProjects] = useState<Project[]>([]);
+  const [projects, setProjects] = useState<Project[]>();
 
   const getProjectList = async () => {
     type JsonData =
@@ -71,7 +71,7 @@ const Projects = () => {
         break;
     }
     getProjectList();
-  }, []);
+  }, [activeFilter, filter]);
   return (
     <main className="min-h-[90vh] w-full relative mt-[80px] flex justify-center">
       <section className="w-full max-w-[1080px] my-10 relative">
@@ -109,71 +109,50 @@ const Projects = () => {
             </div>
           )}
 
-          {[
-            {
-              title: "",
-              description: "",
-              thumbnail: "",
-              sourceCodeLink: "",
-              previewLink: "",
-            },
-            {
-              title: "",
-              description: "",
-              thumbnail: "",
-              sourceCodeLink: "",
-              previewLink: "",
-            },
-            {
-              title: "",
-              description: "",
-              thumbnail: "",
-              sourceCodeLink: "",
-              previewLink: "",
-            },
-          ].map((Item, index) => {
-            return (
-              <div
-                key={index}
-                className="w-full md:max-w-[540px] flex relative flex-1"
-              >
-                <Image
-                  src={Item.thumbnail || "/placeholder.jpg"} // Fallback image
-                  fill
-                  alt={Item.title || "Project image"}
-                  className="absolute w-full h-full overflow-hidden"
-                />
-                <div className="bg-gray-200 rounded-md border border-gray-200 shadow-md w-full">
-                  <div className="card-body w-full max-w-[42rem] bg-slate-200 opacity-40 hover:opacity-95 duration-200">
-                    <h5 className="card-title mb-2.5 p-4 text-[#342c3d76]">
-                      {Item.title || "Untitled Project"}
-                    </h5>
-                    <p className="mb-4 p-4 text-justify text-lg text-[#342c3d76]">
-                      {Item.description || "No description available."}
-                    </p>
-                    <div className="card-actions py-4 flex gap-2">
-                      {Item.sourceCodeLink && (
-                        <Link
-                          href={Item.sourceCodeLink}
-                          className="btn text-sm md:text-lg text-[#342c3d76] border flex-1 border-gray-400"
-                        >
-                          Source code
-                        </Link>
-                      )}
-                      {Item.previewLink && (
-                        <Link
-                          href={Item.previewLink}
-                          className="btn text-sm md:text-lg btn-gradient btn-primary flex-1"
-                        >
-                          Live preview
-                        </Link>
-                      )}
+          {projects &&
+            projects.map((Item, index) => {
+              return (
+                <div
+                  key={index}
+                  className="w-full md:max-w-[540px] flex relative flex-1"
+                >
+                  <Image
+                    src={Item.thumbnail || "/placeholder.jpg"} // Fallback image
+                    fill
+                    alt={Item.title || "Project image"}
+                    className="absolute w-full h-full overflow-hidden"
+                  />
+                  <div className="bg-gray-200 rounded-md border border-gray-200 shadow-md w-full">
+                    <div className="card-body w-full max-w-[42rem] bg-slate-200 opacity-40 hover:opacity-95 duration-200">
+                      <h5 className="card-title mb-2.5 p-4 text-[#342c3d76]">
+                        {Item.title || "Untitled Project"}
+                      </h5>
+                      <p className="mb-4 p-4 text-justify text-lg text-[#342c3d76]">
+                        {Item.description || "No description available."}
+                      </p>
+                      <div className="card-actions py-4 flex gap-2">
+                        {Item.sourceCodeLink && (
+                          <Link
+                            href={Item.sourceCodeLink}
+                            className="btn text-sm md:text-lg text-[#342c3d76] border flex-1 border-gray-400"
+                          >
+                            Source code
+                          </Link>
+                        )}
+                        {Item.previewLink && (
+                          <Link
+                            href={Item.previewLink}
+                            className="btn text-sm md:text-lg btn-gradient btn-primary flex-1"
+                          >
+                            Live preview
+                          </Link>
+                        )}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
       </section>
     </main>
